bdc3a308af1616f41844962bd30214c7f2e97cc6
msiviero <m.siviero83@gmail.com>
2017-01-06 14:52:01 +0100

    eslint styles, make dispatcher aware of what store to dispatch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import _ from 'lodash';
-import React, { Component } from 'react';
+import React from 'react';
 
 const stores = {};
 const bindings = {};
-
 
 const dispatcher = (() => {
   return {
@@ -18,15 +17,18 @@ const dispatcher = (() => {
     unbind: (store, component) => {
       _.remove(bindings[store], item => item === component);
     },
-    dispatch: (action, data) => {
-      for (let store of _.values(stores)) {
-        const actionFn = _.get(store, 'actions.' + action);
-        if (_.isFunction(actionFn)) {
-          store.data = _.assign(store.data, actionFn.call(null, store.data, data));
-          for (let component of bindings[store.name]) {
-            component.setState(store.data);
-          }
-        }
+    dispatch: (store, action, data) => {
+      const instance = stores[store];
+      if (!instance) {
+        throw new Error(`Store ${store} doesn't exist`);
+      }
+      const actionFn = _.get(instance, 'actions.' + action);
+      if (!_.isFunction(actionFn)) {
+        throw new Error(`Store ${store} doesn't have a ${action} action`);
+      }
+      instance.data = _.assign(instance.data, actionFn.call(null, instance.data, data));
+      for (let component of bindings[store]) {
+        component.setState(instance.data);
       }
     }
   };
@@ -39,7 +41,7 @@ function bindStores(WrappedComponent, storesToBind) {
     throw new Error('Stores is not an array');
   }
 
-  return class StoreWrapper extends Component {
+  return class StoreWrapper extends React.Component {
     componentWillMount() {
       for (let store of storesToBind) {
         dispatcher.bind(store, this);
@@ -51,12 +53,12 @@ function bindStores(WrappedComponent, storesToBind) {
       }
     }
     render() {
-      return <WrappedComponent {...(_.assign({}, this.state)) } />
+      return <WrappedComponent {...(_.assign({}, this.state)) } />;
     }
-  }
-};
+  };
+}
 
 export {
   dispatcher,
   bindStores
-}
+};
